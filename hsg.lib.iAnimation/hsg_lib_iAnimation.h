230efc914362b21768356e5c7c1fe427d749c8cb
iTBoyer <iTBoyer@qq.com>
2020-05-13 20:06:18 +0800

    framework

---
+++ hsg.lib.iAnimation/hsg_lib_iAnimation.h
@@ -0,0 +1,19 @@
+//
+//  hsg_lib_iAnimation.h
+//  hsg.lib.iAnimation
+//
+//  Created by jhmac on 2020/5/13.
+//  Copyright © 2020 iTBoyer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for hsg_lib_iAnimation.
+FOUNDATION_EXPORT double hsg_lib_iAnimationVersionNumber;
+
+//! Project version string for hsg_lib_iAnimation.
+FOUNDATION_EXPORT const unsigned char hsg_lib_iAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hsg_lib_iAnimation/PublicHeader.h>
+
+
